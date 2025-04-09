c7b9c31125a0815dcc346782034aa31495892064
c-santos <jcms121899@gmail.com>
2025-04-10 00:40:20 +0800

    fixed exposing django from docker container

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
 set -e
 set -x
 
-docker compose down
-docker compose up -d
-
-docker compose run --rm taskman python manage.py migrate
-
-docker compose logs -f
+docker compose down;
+docker compose up --build -d;
+docker compose run --rm taskman python manage.py migrate;
+docker compose logs -f;
