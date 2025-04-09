e0ae318d4197973a2ce2292c3534cce0a9ecce56
c-santos <jcms121899@gmail.com>
2025-04-10 02:38:27 +0800

    added task detail urls

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from tasks import views
 
 urlpatterns = [
-    path('', views.TaskList.as_view())
+    path('', views.TaskList.as_view()),
+    path('<int:pk>', views.TaskDetail.as_view())
 ]
