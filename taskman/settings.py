37f2de2904f9dfd28598a60cd7cc63646071a16f
c-santos <jcms121899@gmail.com>
2025-04-10 21:55:28 +0800

    enabled cors origin for frontend devt

--- taskman/settings.py
+++ taskman/settings.py
@@ -29,8 +29,11 @@ SECRET_KEY = 'django-insecure-1b0zw)-&y0z^ndure13zp&2%=io@_*-$=evsnpzlyv_^lnjac!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000'
+]
 
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -42,12 +45,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'tasks'
+    'tasks',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
